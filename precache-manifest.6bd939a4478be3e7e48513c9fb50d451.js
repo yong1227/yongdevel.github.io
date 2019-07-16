cfc00f18a0239cc99cec402925df4633c74b801b
yong1227 <rnjs1227@naver.com>
2019-07-17 01:19:49 +0900

    Updates

---
+++ precache-manifest.6bd939a4478be3e7e48513c9fb50d451.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08492a2b42902f8df6acdfd8c32c587",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "db7e45d18e297f9f4d23",
+    "url": "/movie_app/static/css/main.21ccacf2.chunk.css"
+  },
+  {
+    "revision": "9dbb26f972df6c48c132",
+    "url": "/movie_app/static/js/2.e08ebf62.chunk.js"
+  },
+  {
+    "revision": "db7e45d18e297f9f4d23",
+    "url": "/movie_app/static/js/main.b650c193.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
